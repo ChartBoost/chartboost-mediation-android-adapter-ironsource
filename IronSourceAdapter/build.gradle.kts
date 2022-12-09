4224ac36d1011b6bb641700163feaff38a00d1f9
Jose Padilla <jose.padilla@chartboost.com>
2022-12-09 12:07:18 -0800

    [HB-4895]: Update contact email. (#15)

--- IronSourceAdapter/build.gradle.kts
+++ IronSourceAdapter/build.gradle.kts
@@ -132,7 +132,7 @@ afterEvaluate {
                         developer {
                             id.set("chartboostmobile")
                             name.set("chartboost mobile")
-                            email.set("mobile@chartboost.com")
+                            email.set("support@chartboost.com")
                         }
                     }
 

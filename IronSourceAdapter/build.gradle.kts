fa2e7259f6374e89c72e37ca92c68baa84210e95
Bichen Wang <bichenwang@gmail.com>
2023-02-06 12:28:30 -0800

    delete extra dependency

--- IronSourceAdapter/build.gradle.kts
+++ IronSourceAdapter/build.gradle.kts
@@ -66,7 +66,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:+")
     "localImplementation"(project(":Helium"))
 
     // For external usage, please use the following production dependency.

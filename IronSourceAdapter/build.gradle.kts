11081d7091a8fc29ceb936a22e781f27561e69ab
Bichen Wang <bichenwang@gmail.com>
2024-05-30 04:12:35 -0700

    Pre 5.0 getting ready for release

--- IronSourceAdapter/build.gradle.kts
+++ IronSourceAdapter/build.gradle.kts
@@ -18,12 +18,19 @@ plugins {
 repositories {
     google()
     mavenCentral()
+    maven("https://cboost.jfrog.io/artifactory/private-chartboost-core/") {
+        credentials {
+            username = System.getenv("JFROG_USER")
+            password = System.getenv("JFROG_PASS")
+        }
+    }
     maven("https://cboost.jfrog.io/artifactory/private-chartboost-mediation/") {
         credentials {
             username = System.getenv("JFROG_USER")
             password = System.getenv("JFROG_PASS")
         }
     }
+    maven("https://cboost.jfrog.io/artifactory/chartboost-core/")
     maven("https://cboost.jfrog.io/artifactory/chartboost-mediation/")
     maven("https://android-sdk.is.com/")
 }
@@ -36,7 +43,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.8.0.0.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.8.0.0.0"
 
         buildConfigField("String", "CHARTBOOST_MEDIATION_IRONSOURCE_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
@@ -65,6 +72,15 @@ android {
         viewBinding = true
         buildConfig = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
@@ -72,7 +88,7 @@ dependencies {
 
     // For external usage, please use the following production dependency.
     // You may choose a different release version.
-    "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.0.0")
+    "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Partner SDK
     implementation("com.ironsource.sdk:mediationsdk:8.0.0")
